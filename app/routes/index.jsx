b2293479054072acf888eeedcc3f51e3c257d14f
Piotr Zawadzki <piotr@zawadzki.net>
2022-11-20 19:40:27 +0100

    adding and displaying notes

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -3,15 +3,15 @@ import {Link} from "@remix-run/react";
 import homeStyles from '~/styles/home.css'
 
 export default function Index() {
-  return (
-      <main id="content">
-        <h1>A better way of keeping track of your notes</h1>
-        <p>Try our early beta and never loose track of your notes again!</p>
-        <p id="cta">
-          <Link to="/notes">Try Now!</Link>
-        </p>
-      </main>
-  );
+    return (
+        <main id="content">
+            <h1>A better way of keeping track of your notes</h1>
+            <p>Try our early beta and never loose track of your notes again!</p>
+            <p id="cta">
+                <Link to="/notes">Try Now!</Link>
+            </p>
+        </main>
+    );
 }
 
 export function links() {
--- app/routes/notes.jsx
+++ app/routes/notes.jsx
@@ -1,7 +1,41 @@
+import NewNote, { links as newNoteLinks} from "~/components/NewNote";
+import NoteList, { links as noteListLinks } from "~/components/NoteList";
+import {getStoredNotes, storeNotes} from "~/data/notes";
+import {redirect} from "@remix-run/node";
+import {useLoaderData} from "@remix-run/react";
+
 export default function NotesPage() {
+    const notes = useLoaderData()
     return (
         <main>
-            <h1>My Notes</h1>
+            <NewNote />
+            <NoteList notes={notes}/>
         </main>
     )
 }
+
+export async function loader() {
+    const notes = await getStoredNotes()
+    return notes
+}
+
+export async function action({request}) {
+    const formData = await request.formData()
+    // const noteData = {
+    //     title: formData.get('title'),
+    //     content: formData.get('content')
+    // }
+    // TODO: Add validation
+    const noteData = Object.fromEntries(formData)
+    const existingNotes = await getStoredNotes()
+    noteData.id = new Date().toISOString()
+    // TODO: change it to better id generator
+    const updatedNotes = existingNotes.concat(noteData)
+    await storeNotes(updatedNotes)
+    return redirect('/notes')
+}
+
+export function links() {
+    return [...newNoteLinks(), ...noteListLinks()]
+}
+
